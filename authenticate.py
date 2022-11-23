348f99620e9c16ec48041b1537e68549ea9cc0c0
Sam McKnight <sam.mcknight@equipmentshare.com>
2022-11-23 16:36:54 -0700

    Refactor functions into class methods

--- authenticate.py
+++ authenticate.py
@@ -1,54 +1,61 @@
-import logging
 import os
 from datetime import datetime, timedelta
+from typing import Dict
+
 from dotenv import load_dotenv
 import requests
 import base64
 import json
 
-load_dotenv()
 
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-AUTH_TOKEN = f"{CLIENT_ID}:{CLIENT_SECRET}".encode("ascii")
-PRODUCTION_URI = os.environ.get('PRODUCTION_URI')
-OAUTH_CONNECT_URI = os.environ.get('OAUTH_CONNECT_URI')
-ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-SCOPE = 'product.compact'
-encoded_token = base64.b64encode(AUTH_TOKEN)
-encoded_string = encoded_token.decode('ascii')
+class Authenticate:
+    load_dotenv()
 
-now = datetime.utcnow()
+    def __init__(self,
+                 scope: str = 'product.compact'):
+        self.client_id = os.environ.get('CLIENT_ID')
+        self.client_secret = os.environ.get('CLIENT_SECRET')
+        self.auth_token = f"{self.client_id}:{self.client_secret}".encode("ascii")
+        self.oauth_connect_uri = os.environ.get('OAUTH_CONNECT_URI')
+        self.access_token = os.environ.get('ACCESS_TOKEN')
+        self.scope = scope
 
-with open('.env', 'r') as f:
-    lines = f.readlines()
+    def get_auth_access_token(self):
+        encoded_token = base64.b64encode(self.auth_token)
+        encoded_string = encoded_token.decode('ascii')
+        now = datetime.utcnow()
 
-old_expires_in = [line for line in lines if 'EXPIRES_IN' in line]
-old_expiration_time_string = datetime.fromisoformat(old_expires_in[0].split('=')[1].replace('\n', ''))
+        with open('.env', 'r') as f:
+            lines = f.readlines()
 
-if now >= old_expiration_time_string:
-    auth_resp = requests.post(OAUTH_CONNECT_URI,
-                              headers={
-                                  "Authorization": f"Basic {encoded_string}",
-                                  "Content-Type": "application/x-www-form-urlencoded"
-                              },
-                              data={
-                                  "grant_type": "client_credentials",
-                                  "scope": SCOPE
-                              })
+        old_expires_in = [line for line in lines if 'EXPIRES_IN' in line]
+        old_expiration_time_string = datetime.fromisoformat(old_expires_in[0].split('=')[1].replace('\n', ''))
 
-    assert auth_resp.status_code == 200
-    auth_response = json.loads(auth_resp.content)
-    auth_token = auth_response['access_token']
-    expires_in_seconds = auth_response['expires_in']
-    EXPIRATION_TIME = (now + timedelta(seconds=expires_in_seconds)).isoformat()
-    with open('.env', 'w') as f:
-        for line in lines:
-            if 'ACCESS_TOKEN' not in line and 'EXPIRES_IN' not in line:
-                f.write(f"{line}\n")
-            elif 'ACCESS_TOKEN' in line:
-                f.write(f"ACCESS_TOKEN={auth_token}\n")
-            elif 'EXPIRES_IN' in line:
-                f.write(f"EXPIRES_IN={EXPIRATION_TIME}\n")
+        if now >= old_expiration_time_string:
+            auth_resp = requests.post(self.oauth_connect_uri,
+                                      headers={
+                                          "Authorization": f"Basic {encoded_string}",
+                                          "Content-Type": "application/x-www-form-urlencoded"
+                                      },
+                                      data={
+                                          "grant_type": "client_credentials",
+                                          "scope": self.scope
+                                      })
 
-logging.info('authentication successful')
+            if auth_resp.status_code != 200:
+                raise Exception(f"Something went wrong when authenticating:\n{auth_resp.reason}")
+            auth_response = json.loads(auth_resp.content)
+            access_token = auth_response['access_token']
+            expires_in_seconds = auth_response['expires_in']
+            EXPIRATION_TIME = (now + timedelta(seconds=expires_in_seconds)).isoformat()
+            with open('.env', 'w') as f:
+                for line in lines:
+                    if 'ACCESS_TOKEN' not in line and 'EXPIRES_IN' not in line:
+                        f.write(line)
+                    elif 'ACCESS_TOKEN' in line:
+                        f.write(f"ACCESS_TOKEN={access_token}\n")
+                        self.access_token = access_token
+                    elif 'EXPIRES_IN' in line:
+                        f.write(f"EXPIRES_IN={EXPIRATION_TIME}")
+            return self.access_token
+        return self.access_token

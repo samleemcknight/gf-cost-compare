2448ab22d7584ecdc50c6d736a68a7b0fdd12075
Sam McKnight <sam.mcknight@equipmentshare.com>
2022-12-02 12:49:00 -0700

    create constants file and helpers dir

---
+++ grocery_api_client/services/helpers/constants.py
@@ -0,0 +1,3 @@
+KROGER_PRODUCTS_URI = 'https://api.kroger.com/v1/products'
+KROGER_LOCATIONS_URI = 'https://api.kroger.com/v1/locations'
+KROGER_OAUTH_URI = 'https://api.kroger.com/v1/connect/oauth2/token'
